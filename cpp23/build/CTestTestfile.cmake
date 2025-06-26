676553dace6c7e7460daa8ded0953c6144a0b0b7
Durant Schoon <durant.schoon@gmail.com>
2025-06-26 07:55:01 -0400

    Remove build artifacts from git tracking

--- cpp23/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/durant/Repos/others/gatl/cpp23
-# Build directory: /Users/durant/Repos/others/gatl/cpp23/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[Phase1Foundation]=] "/Users/durant/Repos/others/gatl/cpp23/build/bin/test_phase1_foundation")
-set_tests_properties([=[Phase1Foundation]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/durant/Repos/others/gatl/cpp23/CMakeLists.txt;33;add_test;/Users/durant/Repos/others/gatl/cpp23/CMakeLists.txt;0;")
